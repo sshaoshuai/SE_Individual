ff61e2fe2eb2abe9e6e3eb3fe2ac029f184fa8c9
Shaoshuai Shi <shaoshuai_1993@163.com>
2016-09-29 16:53:31 +0800

    Add the function to produce negative number, and comparison function

--- BigNumber/main.cpp
+++ BigNumber/main.cpp
@@ -12,22 +12,29 @@ const int BASE[SCALE] = { 1, 10, 100, 1000 };
 class BigNumber{
 private:
 	int size;
-	int sign;
+	int sign;		// -1: negative, 1: not negative
 	int d[MAXL];	// d[1]: lowest digit
 
 public:
 	BigNumber(){
-		size = 0;
+		size = 0; sign = 1;
 	}
 	// initialize the class from a number in string
-	BigNumber(string s){
-		int len = s.length();
+	BigNumber(string src){
+		if (src == "") src = "0";
+		int len = src.length();
 		memset(d, 0, sizeof(d));
-		size = (len - 1) / SCALE + 1;
-		for (int i = len - 1; i >= 0; i--){
+		if (src[0] == '-'){
+			sign = -1; size = (len - 1 - 1) / SCALE + 1;
+		}
+		else{
+			sign = 1; size = (len - 1) / SCALE + 1;
+		}
+		int MSB = (sign == 1) ? 0 : 1;
+		for (int i = len - 1; i >= MSB; i--){
 			int pos = (len - i - 1) / SCALE + 1;
 			int k = (len - i - 1) % SCALE;
-			d[pos] += BASE[k] * (s[i] - '0');
+			d[pos] += BASE[k] * (src[i] - '0');
 		}
 		while (size > 1 && d[size] == 0) size--;
 	}
@@ -54,12 +61,12 @@ public:
 				curNum %= BASE[k];
 			}
 		}
+		if (sign == -1) ans = '-' + ans;
 		return ans;
 	}
-
-	// BigNumber Add Operation
-	friend BigNumber operator + (const BigNumber & A, const BigNumber & B){
-		BigNumber ans;
+	
+	// add operation, don't care the sign
+	friend void Add(const BigNumber & A, const BigNumber & B, BigNumber & ans){
 		int delta = 0;
 		ans.size = max(A.size, B.size);
 		for (int i = 1; i <= ans.size; i++){
@@ -71,23 +78,95 @@ public:
 			ans.d[++ans.size] = delta % MAX_BASE;
 			delta /= MAX_BASE;
 		}
-		return ans;
 	}
 
-	// BigNumber  Minus Operation
-	friend BigNumber operator - (const BigNumber & A, const BigNumber & B){
-		BigNumber ans;
+	// minus operation, don't care the sign
+	friend void Minus(const BigNumber & A, const BigNumber & B, BigNumber & ans){
 		int delta = 0;
-		ans.size = A.size;
+		ans.size = A.size; ans.sign = 1;
 		for (int i = 1; i <= ans.size; i++){
 			delta = MAX_BASE + A.d[i] - B.d[i] + delta;
 			ans.d[i] = delta % MAX_BASE;
 			delta = delta / MAX_BASE - 1;
 		}
 		while (ans.size > 1 && ans.d[ans.size] == 0) ans.size--;
+	}
+
+	// abs comparison
+	friend bool abs_smaller(const BigNumber & A, const BigNumber & B){
+		if (A.size != B.size) return A.size < B.size;
+		for (int i = A.size; i > 0; i--)
+		if (A.d[i] != B.d[i]) {
+			return A.d[i] < B.d[i];
+		}
+		return false;
+	}
+
+	// BigNumber Add Operation
+	friend BigNumber operator + (const BigNumber & A, const BigNumber & B){
+		BigNumber ans;
+		if (A.sign == 1 && B.sign == -1){
+			// A - B
+			if (abs_smaller(A, B)){
+				// |A| < |B|
+				Minus(B, A, ans); ans.sign = -1;
+			}
+			else{
+				Minus(A, B, ans); ans.sign = 1;
+			}
+			return ans;
+		}
+		if (A.sign == -1 && B.sign == 1){
+			// B - A
+			if (abs_smaller(B, A)){
+				Minus(A, B, ans); ans.sign = -1;
+			}
+			else{
+				Minus(B, A, ans); ans.sign = 1;
+			}
+			return ans;
+		}
+		Add(A, B, ans);
+		ans.sign = A.sign;
 		return ans;
 	}
 
+	// BigNumber  Minus Operation
+	friend BigNumber operator - (const BigNumber & A, const BigNumber & B){
+		BigNumber ans;
+		if (A.sign == 1 && B.sign == 1){
+			if (abs_smaller(A, B)){
+				Minus(B, A, ans); ans.sign = -1;
+			}
+			else{
+				Minus(A, B, ans); ans.sign = 1;
+			}
+			return ans;
+		}
+		if (A.sign == -1 && B.sign == -1){
+			if (abs_smaller(B, A)){
+				Minus(A, B, ans); ans.sign = -1;
+			}
+			else{
+				Minus(B, A, ans); ans.sign = 1;
+			}
+			return ans;
+		}
+		Add(A, B, ans);
+		ans.sign = A.sign;
+		return ans;
+	}
+
+	// A < B comparison
+	friend bool operator < (const BigNumber & A, const BigNumber & B){
+		if (A.sign != B.sign){
+			if (A.sign == -1) return true;
+			return false;
+		}
+		if (A.sign == -1) 
+			return !abs_smaller(A, B);
+		return abs_smaller(A, B);
+	}
 
 };
 
@@ -96,10 +175,11 @@ public:
 
 int main()
 {
-	BigNumber A("13543541646588824354350");
-	BigNumber B("41353125343152433152345");
-	BigNumber C("41353125343152433152344");
-	cout << (B - C).toString() << endl;
+	BigNumber A("-13543541646588824354350");
+	BigNumber B("-41353125343152433152345");
+	BigNumber C("22441353125343152433152344");
+	cout << (A + B).toString() << endl;
+	cout << (A - C).toString() << endl;
 	getchar();
 	return 0;
 }
